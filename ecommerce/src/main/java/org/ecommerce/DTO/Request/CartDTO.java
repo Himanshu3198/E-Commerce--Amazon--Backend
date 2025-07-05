913e49ddb4cb0d504bff94f34eee7ad497411078
Himanshu <himanshu3198live@gmail.com>
2025-07-05 13:23:31 +0530

    Cart Controller completed

--- ecommerce/src/main/java/org/ecommerce/DTO/Request/CartDTO.java
+++ ecommerce/src/main/java/org/ecommerce/DTO/Request/CartDTO.java
@@ -1,12 +1,20 @@
 package org.ecommerce.DTO.Request;
 
-import org.ecommerce.Entity.CartItem;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.ecommerce.Entity.Product;
+
 
 import java.util.List;
 
 public record CartDTO(
+        @NotNull(message = "useId cannot be null")
+        @JsonProperty(value = "userId")
         Long userId,
-        List<CartItemDTO> cartItems,
-        Double totalAmount,
-        Double discount
+        @JsonProperty(value = "productId")
+        Long productId,
+        @Min(value = 1)
+        @JsonProperty(value = "quantity")
+        Long quantity
 ) {}
